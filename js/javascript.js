35500a20b7cc7b55f59fcb99e648253f9c302a4e
masonhill392 <masonhill392@gmail.com>
2022-11-03 18:30:28 -0800

    Template - can be further optimized

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,56 @@
 console.log('js connect');
+var string = "."; 
+var lineone = ".";
+var linetwo = ".";
+var linethree = ".";
+var linefour = ".";
+var linefive = ".";
+
+
+$("#button1").click(function() {
+    movelines();
+    string = "cat";
+    updateMessageDisplay();
+});
+$("#button2").click(function() {
+    movelines();
+    string = "dog";
+    updateMessageDisplay();
+
+});
+$("#button3").click(function() {
+    movelines();
+    string = "cow";
+    updateMessageDisplay();
+
+});
+$("#button4").click(function() {
+    movelines();
+    string = "horse";
+    updateMessageDisplay();
+
+});
+$("#button5").click(function() {
+    movelines();
+    string = "pig";
+    updateMessageDisplay();
+
+});
+function updateMessageDisplay() {
+    lineone = string;
+    $("#line1").html("1: " + lineone);
+    $("#line2").html("2: " + linetwo);
+    $("#line3").html("3: " + linethree);
+    $("#line4").html("4: " + linefour);
+    $("#line5").html("5: " + linefive);
+};
+function movelines() {
+    linefive = linefour;
+    linefour = linethree
+    linethree = linetwo;
+    linetwo = lineone;
+}
+
+
+
+
